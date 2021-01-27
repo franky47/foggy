b30925348d7f0d0c82a0dc368081dedaabc4ac63
Francois Best <francois@francoisbest.com>
2021-01-27 19:41:18 +0100

    feat: Add Sentry for error monitoring

--- src/pages/api/og.ts
+++ src/pages/api/og.ts
@@ -1,5 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import got from 'got'
+import * as Sentry from '@sentry/node'
+import '@sentry/tracing'
 
 // --
 
@@ -10,11 +12,18 @@ interface FigmaAPIResult {
   }
 }
 
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+})
+
 // Example:
 // https://www.figma.com/file/LCzKwbepwQ5MaPwDJ8FCUsRo/francoisbest.com?node-id=301%3A7
 const FIGMA_REGEX = /^https:\/\/www\.figma\.com\/file\/([\w]*).+node-id=([\w%:]+)(\&.*)?$/
 
-export async function processFigmaUrl(figmaURL: string) {
+export async function processFigmaURL(figmaURL: string) {
   const match = figmaURL.match(FIGMA_REGEX)
   if (!match) {
     throw new Error('Invalid Figma URL')
@@ -38,12 +47,19 @@ export async function processFigmaUrl(figmaURL: string) {
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
   const url = req.query.url as string
+  const tx = Sentry.startTransaction({
+    op: 'processFigmaURL',
+    name: 'Process Figma URL',
+  })
   try {
-    const result = await processFigmaUrl(url)
+    const result = await processFigmaURL(url)
     res.redirect(result)
   } catch (error) {
+    Sentry.captureException(error)
     res.status(400).json({
       error: error.message,
     })
+  } finally {
+    tx.finish()
   }
 }
