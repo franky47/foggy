f6f32588c3929ec764d80c672ba9edf198aae035
Francois Best <francois@francoisbest.com>
2021-01-27 16:51:05 +0100

    feat: Add emoji favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,13 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link
+            rel="icon"
+            type="image/svg+xml"
+            href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 width=%22256%22 height=%22256%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 dominant-baseline=%22central%22 text-anchor=%22middle%22 font-size=%2290%22>🌁</text></svg>"
+          />
+        </Head>
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
