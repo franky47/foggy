f4d2e75462f99b67d87b4ee6861defb2050383f9
Francois Best <francois@francoisbest.com>
2021-01-27 12:05:50 +0100

    chore: Test dogfood

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,13 @@
+import { DefaultSeo } from 'next-seo'
+import seo from 'src/seo.json'
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <DefaultSeo {...seo} />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,6 @@
-import Head from 'next/head'
-
 export default function Home() {
   return (
     <main>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <h1>Foggy</h1>
     </main>
   )
