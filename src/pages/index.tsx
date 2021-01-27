1d461e37f25d00d6bbc0b477dc2d73e14f4f5731
Francois Best <francois@francoisbest.com>
2021-01-27 11:07:01 +0100

    start: Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <main>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Foggy</h1>
+    </main>
+  )
+}
