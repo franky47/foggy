b6f018779dbba973fe921ee9f914e07043643ca7
Francois Best <francois@francoisbest.com>
2021-01-28 15:55:43 +0100

    chore: Add missing type defs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,9 @@ import { DefaultSeo } from 'next-seo'
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from 'src/ui/theme'
 import seo from 'src/seo.json'
+import { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <DefaultSeo {...seo} />
